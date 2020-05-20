727a7eff802fdd75d428aabca576a0e387a3116d
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-05-20 16:51:01 -0300

    Create frontend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/',(request, response) =>{
-    return response.send('<h1>Hello word</h1>')
+app.use(express.json);
+
+app.get('/users',(request, response) =>{
+    const dados = request.query;
+    console.log(dados);
+
+    return response.send('<h1>Hello word</h1>');
 });
-app.listen(8000);
+app.listen(3333);
