bf1d542c675f69c77cf722fbc3e7d98210acbc5e
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-05-27 18:58:46 -0300

    update caso page
    I finished the connection with the api on the caso page

--- frontend/src/pages/Caso/index.js
+++ frontend/src/pages/Caso/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css'
 import   {FiArrowLeft}   from   'react-icons/fi' 
 import Logo from '../../assets/logo.svg';
-import {Link} from 'react-router-dom'; 
+import {Link, useHistory} from 'react-router-dom'; 
+import api from '../../services/api';
 
 export default function Caso(){
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    const ong_id = localStorage.getItem('ongId');
+    const history = useHistory();
+    async function cad(e){
+        e.preventDefault();
+        const dados = {
+            title,
+            description,
+            value,  
+        };
+        await api.post('/incidents', dados, {
+            headers: {
+                authorization: ong_id,
+            }
+        })
+        history.push('/list');
+    }
     return(
         <div className="container-cadastro">
             <div className="content">
@@ -17,10 +37,10 @@ export default function Caso(){
                         Voltar para home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Título do caso" />
-                    <textarea placeholder="Descrição" className="descricao" />
-                    <input placeholder="Valor em reais" />
+                <form onSubmit={cad}>
+                    <input placeholder="Título do caso" value={title} onChange={e => setTitle(e.target.value)}/>
+                    <textarea placeholder="Descrição" className="descricao"  value={description} onChange={e => setDescription(e.target.value)}/>
+                    <input placeholder="Valor em reais" value={value} onChange={e => setValue(e.target.value)} />
                     <div className="input-group">
                         <button type="button" className="cancelar" color="#E02041"> Cancelar </button>
                         <button type="submit" className="button">Cadastrar</button>
--- frontend/src/pages/Listar/index.js
+++ frontend/src/pages/Listar/index.js
@@ -1,76 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles.css';
 import {FiPower} from 'react-icons/fi';
 import {FiTrash2} from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import {Link,useHistory} from 'react-router-dom';
+import api from '../../services/api';
 
 export default function Listar(){
+    const [incidents, setIncidents] = useState([]);
     const ong = localStorage.getItem('nameOng');
+    const id = localStorage.getItem('ongId')
     const history = useHistory();
+    async function deletar(id){
+        await api.delete("/incidents/"+id, {
+            headers: {
+                authorization:id,
+            }
+        });
+        setIncidents(incidents.filter(incident=> incidents.id != id ));
+    }
     function deslogar(){
+        localStorage.clear();
         history.push('/');
     }
+
+    useEffect(()=>{
+        api.get('/incidents', {
+            headers:{
+                authorization: id,
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [id])
     return(
         <div className="container-listar">
             <header>
             <img src={logoImg} alt= "be the hero"/>
             <p>Bem vindo, {ong}</p>
             <Link className="button" to="/casos/novo">Cadastrar novo caso</Link>
-            <button type="button"> 
-                <FiPower size={18} color="#E02041" onClick={deslogar}></FiPower>
+            <button type="button" onClick={deslogar}> 
+                <FiPower size={18} color="#E02041"></FiPower>
             </button>
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
-                <li>
+               {incidents.map(incident => (
+                    <li>
                     <strong>CASO:</strong>
-                    <p>Caso teste</p>
+                    <p>{incidents.title}</p>
 
                     <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
+                    <p>{incidents.description}</p>
 
                     <strong>VALOR:</strong>
-                    <p>R$120,00</p>
+                    <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incidents.value)}</p>
 
-                    <button type="button"><FiTrash2 size={20} color="#a8a8b3"></FiTrash2></button>
+                    <button type="button" onClick={()=>deletar(incidents.id)}><FiTrash2 size={20} color="#a8a8b3"></FiTrash2></button>
                 </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="#a8a8b3"></FiTrash2></button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="#a8a8b3"></FiTrash2></button>
-                </li>
-                <li>
-                    <strong>CASO:</strong>
-                    <p>Caso teste</p>
-
-                    <strong>DESCRIÇÃO:</strong>
-                    <p>Descrição teste</p>
-
-                    <strong>VALOR:</strong>
-                    <p>R$120,00</p>
-
-                    <button type="button"><FiTrash2 size={20} color="#a8a8b3"></FiTrash2></button>
-                </li>
+               ))}
+                
             </ul>
         </div>
     );
