82eac769d2203eeb2e865b077ac6d855c39153b6
Pedro Gonçalves dos Santos <pedrogsantos_10@hotmail.com>
2020-11-09 01:46:09 -0300

    Creating git ignore

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import Logo from '../../assets/logo.svg';
-import   {FiArrowLeft}   from   'react-icons/fi' 
-import {Link, useHistory} from 'react-router-dom'; 
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link, useHistory } from 'react-router-dom';
 import api from '../../services/api';
-export default function Cadastro(){
+export default function Cadastro() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [whatsapp, setWhatsapp] = useState('');
     const [city, setCidade] = useState('');
     const [uf, setUF] = useState('');
     const history = useHistory();
-    async function envio(e){
+    async function envio(e) {
         e.preventDefault();
         const dados = {
             name,
@@ -19,22 +19,22 @@ export default function Cadastro(){
             whatsapp,
             city,
             uf,
-         };
-       
-            const response = await api.post('/ongs',dados);
-            alert("Seu id é:"+response.data.id);
-            history.push('/');
-     
+        };
+
+        const response = await api.post('/ongs', dados);
+        alert("Seu id é:" + response.data.id);
+        history.push('/');
+
     }
-    return(
+    return (
         <div className="container-cadastro">
             <div className="content">
                 <section>
-                    <img src={Logo} alt="Be the hero"/>
+                    <img src={Logo} alt="Be the hero" />
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ong</p>
                     <Link className="back-link" to="/">
-                        <FiArrowLeft size={16} color="#E02041"/>
+                        <FiArrowLeft size={16} color="#E02041" />
                         Voltar para o logon
                     </Link>
                 </section>
@@ -44,7 +44,7 @@ export default function Cadastro(){
                     <input placeholder="Whatsapp" value={whatsapp} onChange={e => setWhatsapp(e.target.value)} />
                     <div className="input-group">
                         <input placeholder="Cidade" value={city} onChange={e => setCidade(e.target.value)} />
-                        <input placeholder="UF" style={{width:80}} className="uf" value={uf} onChange={e => setUF(e.target.value)} />
+                        <input placeholder="UF" style={{ width: 80 }} className="uf" value={uf} onChange={e => setUF(e.target.value)} />
                     </div>
                     <button type="submit" className="button">Cadastrar</button>
                 </form>
